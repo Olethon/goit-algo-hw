b24603a818f0f7b463c66cdd8f7ffa6e148c18e2
Oleg <kasyanenko18051990@gmail.com>
2024-01-12 00:12:54 +0200

    test

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-def format_string(string, length):
-    number_of_seats = (length - len(string)) // 2
-    if len(string) < int(length):
-        return f"{'9'*number_of_seats}{string}"
-    else:
-        return string
-    
-
-print(format_string("12345", 15))
+print("hellow boy")
