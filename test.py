91f016f139b4c6a50c50b16d1efc7d188742df7b
Oleg <kasyanenko18051990@gmail.com>
2024-01-12 00:06:14 +0200

    test

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-name = "Daniel" 
-age = 23
+def format_string(string, length):
+    number_of_seats = (length - len(string)) // 2
+    if len(string) < int(length):
+        return f"{'9'*number_of_seats}{string}"
+    else:
+        return string
+    
+
+print(format_string("12345", 15))
